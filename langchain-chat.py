8074864fad0039b20837bb07d439c8bdd9f87336
Rythm <rythmrobin@gmail.com>
2024-06-08 21:45:09 +0530

    added code

---
+++ langchain-chat.py
@@ -0,0 +1,35 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+import streamlit as st
+from dotenv import load_dotenv
+import langchain
+
+
+load_dotenv()
+
+i = 0
+#make a chat prompt
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please response to the user queries"),
+        ("user","Question:{question}")
+    ]
+)
+
+# streamlit framework
+st.title('Langchain Demo With LLAMA2')
+input_text=st.text_input("Type in here to search...", key=f"unique_key{i}")
+
+# ollama llama2 
+llm=Ollama(model="llama2")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+# write output to screen
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
+
+
+
