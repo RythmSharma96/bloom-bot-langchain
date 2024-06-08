3c043390d2fcd59af699f65203378cc79c162e2c
Rythm <rythmrobin@gmail.com>
2024-06-08 22:03:27 +0530

    removed langchain import

--- langchain-chat.py
+++ langchain-chat.py
@@ -4,7 +4,6 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_community.llms import Ollama
 import streamlit as st
 from dotenv import load_dotenv
-import langchain
 
 
 load_dotenv()
